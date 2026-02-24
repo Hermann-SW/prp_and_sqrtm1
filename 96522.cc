6df8252740f8c618624a5d669206ee4f4393817f
Hermann-SW <hermann@stamm-wilbrandt.de>
2026-02-24 20:09:37 +0100

    96,522 decimal digits Carmichael number from @Neptune

---
+++ 96522.cc
@@ -0,0 +1,38 @@
+// g++ -O3 96522.cc paulunderwood/gw_utility.o -lgmp -lgmpxx -Lp95v3019b14/gwnum -l:gwnum.a
+//
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <gmpxx.h>
+#include <iostream>
+
+extern "C" {
+#include "paulunderwood/gw_utility.h"
+}
+
+typedef uint64_t ut;
+
+int main(int argc, char *argv[]) {
+  mpz_class p43;
+  mpz_primorial_ui(p43.get_mpz_t(), 43);
+
+  mpz_class p = (5752211*p43/2-1);
+  mpz_pow_ui(p.get_mpz_t(), p.get_mpz_t(), 1069);
+  mpz_class q = 3*p+1;
+  p = 3*p/2+1;
+
+  mpz_class a = (5752211*p43/2-1);
+  mpz_pow_ui(a.get_mpz_t(), a.get_mpz_t(), 1069);
+  mpz_class r = 3*(a*(a+1))/1050650772710+1;
+
+  mpz_class n = p*q*r;
+  mpz_class n1 = n-1;
+
+  a = 2;
+  gw_powm(a.get_mpz_t(), a.get_mpz_t(), n1.get_mpz_t(), n.get_mpz_t());
+
+  std::cout << a << "\n";
+
+  return 0;
+}
