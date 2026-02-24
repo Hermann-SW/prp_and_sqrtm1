8f59acb6074e4b95bcfeef899a0f32f4c3722a6c
Hermann-SW <hermann@stamm-wilbrandt.de>
2026-02-24 21:32:57 +0100

    New _() macro to hide use of .get_mpz_t(), now cpplinted

--- 96522.cc
+++ 96522.cc
@@ -1,8 +1,11 @@
+// NOLINT(legal/copyright)
+//
 // @Neptune's largest known 3-Carmichael number:
 // https://www.mersenneforum.org/node/22080/page3#post1066763
-//
-// g++ -O3 96522.cc paulunderwood/gw_utility.o -lgmp -lgmpxx -Lp95v3019b14/gwnum -l:gwnum.a
-//
+/*
+   LIBS="-lgmp -lgmpxx -Lp95v3019b14/gwnum -l:gwnum.a"
+   g++ -O3 96522.cc paulunderwood/gw_utility.o $LIBS
+*/
 #include <gmpxx.h>
 #include <iostream>
 
@@ -10,12 +13,14 @@ extern "C" {
 #include "paulunderwood/gw_utility.h"
 }
 
+#define _(v) (v).get_mpz_t()
+
 int main(int argc, char *argv[]) {
   mpz_class p43;
-  mpz_primorial_ui(p43.get_mpz_t(), 43);
+  mpz_primorial_ui(_(p43), 43);
 
   mpz_class a = 5752211*p43/2-1;
-  mpz_pow_ui(a.get_mpz_t(), a.get_mpz_t(), 1069);
+  mpz_pow_ui(_(a), _(a), 1069);
 
   mpz_class p = 3*a/2+1;
   mpz_class q = 3*a+1;
@@ -26,7 +31,7 @@ int main(int argc, char *argv[]) {
 
   srandom(time(NULL));
   a = 2+random();
-  gw_powm(a.get_mpz_t(), a.get_mpz_t(), n1.get_mpz_t(), n.get_mpz_t());
+  gw_powm(_(a), _(a), _(n1), _(n));
 
   std::cout << a << "\n";
 
